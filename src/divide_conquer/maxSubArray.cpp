682e7da6d337e286513dee1db009b45652cd2ee4
TianpeiLuke <tianpei@umich.edu>
2017-01-20 08:59:54 -0500

    change maxSubArray

--- src/divide_conquer/maxSubArray.cpp
+++ src/divide_conquer/maxSubArray.cpp
@@ -58,31 +58,6 @@ int maxSubArrayAuxiliary(vector<int>& nums, int startIdx, int endIdx){
 }
 
 
-
-int maxSubArray_v2(vector<int>& nums){
-/*
-    using dynamic programming, with O(n) time complexity and additional O(n) space complexity
-*/
-   int maxAllSub = nums[0];
-   /* define maxSubEndAt[k] to be the sum of  largest subarray that ends at k
-      the final result should be maximum of list maxSubEndAt
-      
-      equivalently, let maxAllSub store the maximum value of maxSubEndAt[0:k]
-      and reduce the array maxSubEndAt as a value, i.e. the sum of largest subarray that ends at k
-      maxSubEndAt[k] = max(maxSubEndAt[k-1]+nums[k], nums[k])
-       i.e. two cases
-              1) attach the new point nums[k] (remain contiguous) and the subarray length + 1
-              2) reset the new point as start (broken contiguous), the subarray length = 1
-   */
-   int maxSubEndAt = nums[0];
-   for(int i=1; i<nums.size(); i++){
-      maxSubEndAt = max(maxSubEndAt + nums[i], nums[i]);
-      maxAllSub = max(maxAllSub, maxSubEndAt);
-   }
-   return maxAllSub;
-}
-
-
 int main(){
    vector<int> nums;
    nums = {-2,1,-3,4,-1,2,1,-5,4};
